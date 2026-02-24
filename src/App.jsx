c5e9752c9bff5e20491b00bd9c88d076bfdf48b8
mendy hershkop <hers.mm@gmail.com>
2026-02-24 11:15:21 +0200

    create dashboard without logick

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Dashboard from './cumponents/Dashboard'
 import Head from './cumponents/Head'
 
 function App() {
@@ -6,6 +7,11 @@ function App() {
   return (
     <>
     <Head></Head>
+    <div className='dateles'>
+    <Dashboard logo={'public/bomb.jpeg'} head={'Bombs Remaining'} content={5}/>
+    <Dashboard logo={'public/timer.jpeg'} head={'Time Remaining'} content={5}/>
+    <Dashboard logo={'public/panel.jpeg'} head={'Board Size'} content={5}/>
+    </div>
     </>
   )
 }
---
+++ src/cumponents/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Dashboard(props) {
+  return (
+    <div className='dasboard'>
+      <section>
+        <img src={props.logo} alt="" />
+        <p>{props.head}</p>
+      </section>
+      <h1>{props.content}</h1>
+    </div>
+  )
+}

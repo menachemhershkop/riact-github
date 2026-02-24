9b3406417e950a6ff9b31f5e022a759e2b72fd04
Idan <idansano10@gmail.com>
2026-02-24 14:56:41 +0200

    comited

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,35 @@ import Timer from './cumponents/Timer'
 function App() {
   const [bomb, setBomb] = useState(5)
   const [timer, setTimer] = useState(150);
-    const [timeInterval, setTimeInterval] = useState(null);
-    const startTimer = () =>{
-        setTimeInterval(setInterval(()=>{
-            setTimer((prev) => prev-1);
-        }, 1000)
-    )}
-    const pauseTimer=()=>{
-        clearInterval(timeInterval);
-    };
-    const resetTimer = ()=>{
-        setTimer(0)
-        clearInterval(timeInterval)
-    }
-    if (timer ==0){
-        pauseTimer()
-    }
-  
+  const [timeInterval, setTimeInterval] = useState(null);
+  const startTimer = () => {
+    setTimeInterval(setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000)
+    )
+  }
+  const pauseTimer = () => {
+    clearInterval(timeInterval);
+  };
+  const resetTimer = () => {
+    setTimer(0)
+    clearInterval(timeInterval)
+  }
+  if (timer == 0) {
+    pauseTimer()
+  }
+
   return (
     <><div className='main-div'>
-    <Head></Head>
-    <div className='dateles'>
-    <Dashboard logo={'public/bomb.jpeg'} head={'Bombs Remaining'} content={bomb}/>
-    <Dashboard logo={'public/timer.jpeg'} head={'Time Remaining'} content={timer}/>
-    <Dashboard logo={'public/panel.jpeg'} head={'Board Size'} content={'10 X 8'}/>
-    </div>
-    <Timer timer={timer} startTimer={startTimer} pauseTimer={pauseTimer} resetTimer={resetTimer}></Timer>
-    <div className='info'>
-      Locate and neutralize all bombs before runs out.
-    </div>
+      <Head></Head>
+      <div className='dateles'>
+        <Dashboard logo={'public/bomb.jpeg'} head={'Bombs Remaining'} content={bomb} />
+        <Dashboard logo={'public/timer.jpeg'} head={'Time Remaining'} content={<Timer />} />
+        <Dashboard logo={'public/panel.jpeg'} head={'Board Size'} content={'10 X 8'} />
+      </div>
+      <Board />
+      </div>
     </>
-  )
+      )
 }
-
-export default App
+      export default App
